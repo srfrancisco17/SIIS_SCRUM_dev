9460275e9c830c918b484dd98be2e7ec4ef7654b
Francisco Andres Ortega Florez <srfrancisco17@gmail.com>
2017-06-07 07:52:44 -0500

    [Commit] Comenzando !!

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,6 +22,7 @@ class AppAsset extends AssetBundle
     ];
     public $js = [
     ];
+    
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
--- views/site/indexScrumMaster.php
+++ views/site/indexScrumMaster.php
@@ -1,22 +1,73 @@
 <?php
-
 /* @var $this yii\web\View */
 
 $this->title = 'Proyecto-FAOF2';
+
+$this->registerJsFile('@web/js/loader.js', ['position' => $this::POS_HEAD]);
+
+//$this->registerJsFile('https://www.gstatic.com/charts/loader.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+
 ?>
-<div class="site-index">
+<!--<script src="/Proyecto-FAOF2/web/js/loader.js"></script>-->
+<!--<script src="https://www.gstatic.com/charts/loader.js"></script>-->
 
-    <div class="jumbotron">
-        <h1>BIENVENIDO SCRUM-MASTER!</h1>
+<div class="body-content">
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['gantt']});
+    google.charts.setOnLoadCallback(drawChart);
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+    function drawChart() {
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Task ID');
+      data.addColumn('string', 'Task Name');
+      data.addColumn('string', 'Resource');
+      data.addColumn('date', 'Start Date');
+      data.addColumn('date', 'End Date');
+      data.addColumn('number', 'Duration');
+      data.addColumn('number', 'Percent Complete');
+      data.addColumn('string', 'Dependencies');
 
-    <div class="body-content">
+      data.addRows([
+        ['2014Spring', 'Spring 2014', 'spring',
+         new Date(2014, 2, 22), new Date(2014, 5, 20), null, 50, null],
+        ['2014Summer', 'Summer 2014', 'summer',
+         new Date(2014, 5, 21), new Date(2014, 8, 20), null, 100, null],
+        ['2014Autumn', 'Autumn 2014', 'autumn',
+         new Date(2014, 8, 21), new Date(2014, 11, 20), null, 100, null],
+        ['2014Winter', 'Winter 2014', 'winter',
+         new Date(2014, 11, 21), new Date(2015, 2, 21), null, 100, null],
+        ['2015Spring', 'Spring 2015', 'spring',
+         new Date(2015, 2, 22), new Date(2015, 5, 20), null, 50, null],
+        ['2015Summer', 'Summer 2015', 'summer',
+         new Date(2015, 5, 21), new Date(2015, 8, 20), null, 0, null],
+        ['2015Autumn', 'Autumn 2015', 'autumn',
+         new Date(2015, 8, 21), new Date(2015, 11, 20), null, 0, null],
+        ['2015Winter', 'Winter 2015', 'winter',
+         new Date(2015, 11, 21), new Date(2016, 2, 21), null, 0, null],
+        ['Football', 'Football Season', 'sports',
+         new Date(2014, 8, 4), new Date(2015, 1, 1), null, 100, null],
+        ['Baseball', 'Baseball Season', 'sports',
+         new Date(2015, 2, 31), new Date(2015, 9, 20), null, 14, null],
+        ['Basketball', 'Basketball Season', 'sports',
+         new Date(2014, 9, 28), new Date(2015, 5, 20), null, 86, null],
+        ['Hockey', 'Hockey Season', 'sports',
+         new Date(2014, 9, 8), new Date(2015, 5, 21), null, 89, null]
+      ]);
 
+      var options = {
+        height: 400,
+        gantt: {
+          trackHeight: 30
+        }
+      };
 
+      var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
 
-    </div>
+      chart.draw(data, options);
+    }
+  </script> 
+  
+   <div id="chart_div"></div>
 </div>
+
