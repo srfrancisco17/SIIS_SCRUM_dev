a6566cdfb3507ae49221d500fd28537dddfb921c
Francisco Andres Ortega Florez <srfrancisco17@gmail.com>
2017-07-04 14:58:59 -0500

    [Commit] - Modificando el inicio de sesion

--- views/site/login.php
+++ views/site/login.php
@@ -1,233 +1,231 @@
 <?php
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \common\models\LoginForm */
-
 $this->title = 'Iniciar sesión';
-
 $fieldOptions1 = [
     'options' => ['class' => 'form-group has-feedback'],
     'inputTemplate' => "{input}<span class='glyphicon glyphicon-user form-control-feedback'></span>"
 ];
-
 $fieldOptions2 = [
     'options' => ['class' => 'form-group has-feedback'],
     'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
 ];
 
-$this->registerJs('
+$this->registerCssFile("@web/css/login.css");
+$this->registerCss("
     
-$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){
-    particlesJS("particles-js",
-      {
-        "particles": {
-          "number": {
-            "value": 80,
-            "density": {
-              "enable": true,
-              "value_area": 800
-            }
-          },
-          "color": {
-            "value": "#ffffff"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 5
-            },
-            "image": {
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-              "enable": false,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 5,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 40,
-              "size_min": 0.1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 6,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "repulse"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "push"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 400,
+    @import url('https://fonts.googleapis.com/css?family=Nunito');
+    @import url('https://fonts.googleapis.com/css?family=Poiret+One');
+    
+    body, html {
+            height: 100%;
+            background-repeat: no-repeat;    /*background-image: linear-gradient(rgb(12, 97, 33),rgb(104, 145, 162));*/
+            background:black;
+            position: relative;
+    }
+    #login-box {
+            position: absolute;
+            top: 150px;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 350px;
+            margin-top: 5%;
+            /*margin: 0 auto;*/
+            border: 1px solid black;
+            background: rgba(48, 46, 45, 1);
+            min-height: 250px;
+            padding: 20px;
+            z-index: 9999;
+
+    }
+    #login-box .logo .logo-caption {
+            font-family: 'Poiret One', cursive;
+            color: white;
+            text-align: center;
+            margin-bottom: 0px;
+    }
+    #login-box .logo .tweak {
+            color: #ff5252;
+    }
+    .login-logo a, .register-logo a{
+        color: white;
+    }
+    #login-box .controls {
+            padding-top: 30px;
+    }
+    #login-box .controls input {
+            border-radius: 0px;
+            background: rgb(98, 96, 96);
+            border: 0px;
+            color: white;
+            font-family: 'Nunito', sans-serif;
+    }
+    #login-box .controls input:focus {
+            box-shadow: none;
+    }
+    #login-box .controls input:first-child {
+            border-top-left-radius: 2px;
+            border-top-right-radius: 2px;
+    }
+    #login-box .controls input:last-child {
+            border-bottom-left-radius: 2px;
+            border-bottom-right-radius: 2px;
+    }
+    #login-box button.btn-custom {
+            border-radius: 2px;
+            margin-top: 8px;
+            background:#ff5252;
+            border-color: rgba(48, 46, 45, 1);
+            color: white;
+            font-family: 'Nunito', sans-serif;
+    }
+    #login-box button.btn-custom:hover{
+            -webkit-transition: all 500ms ease;
+            -moz-transition: all 500ms ease;
+            -ms-transition: all 500ms ease;
+            -o-transition: all 500ms ease;
+            transition: all 500ms ease;
+            background: rgba(48, 46, 45, 1);
+            border-color: #ff5252;
+    }
+    #particles-js{
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: 50% 50%;
+            position: fixed;
+            background-color: #1D1F20;
+            top: 0px;
+            z-index:1;
+    }    
+");
+
+$this->registerJs('
+//<!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></script>-->
+    $.getScript("'.Yii::getAlias('@web').'/js/particles.min.js", function(){
+        particlesJS("particles-js",
+          {
+            "particles": {
+              "number": {
+                "value": 80,
+                "density": {
+                  "enable": true,
+                  "value_area": 800
+                }
+              },
+              "color": {
+                "value": "#ffffff"
+              },
+              "shape": {
+                "type": "circle",
+                "stroke": {
+                  "width": 0,
+                  "color": "#000000"
+                },
+                "polygon": {
+                  "nb_sides": 5
+                },
+                "image": {
+                  "width": 100,
+                  "height": 100
+                }
+              },
+              "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                  "enable": false,
+                  "speed": 1,
+                  "opacity_min": 0.1,
+                  "sync": false
+                }
+              },
+              "size": {
+                "value": 5,
+                "random": true,
+                "anim": {
+                  "enable": false,
+                  "speed": 40,
+                  "size_min": 0.1,
+                  "sync": false
+                }
+              },
               "line_linked": {
-                "opacity": 1
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+              },
+              "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                  "enable": false,
+                  "rotateX": 600,
+                  "rotateY": 1200
+                }
               }
             },
-            "bubble": {
-              "distance": 400,
-              "size": 40,
-              "duration": 2,
-              "opacity": 8,
-              "speed": 3
+            "interactivity": {
+              "detect_on": "canvas",
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+                },
+                "onclick": {
+                  "enable": true,
+                  "mode": "push"
+                },
+                "resize": true
+              },
+              "modes": {
+                "grab": {
+                  "distance": 400,
+                  "line_linked": {
+                    "opacity": 1
+                  }
+                },
+                "bubble": {
+                  "distance": 400,
+                  "size": 40,
+                  "duration": 2,
+                  "opacity": 8,
+                  "speed": 3
+                },
+                "repulse": {
+                  "distance": 200
+                },
+                "push": {
+                  "particles_nb": 4
+                },
+                "remove": {
+                  "particles_nb": 2
+                }
+              }
             },
-            "repulse": {
-              "distance": 200
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
+            "retina_detect": true,
+            "config_demo": {
+              "hide_card": false,
+              "background_color": "#b61924",
+              "background_image": "",
+              "background_position": "50% 50%",
+              "background_repeat": "no-repeat",
+              "background_size": "cover"
             }
           }
-        },
-        "retina_detect": true,
-        "config_demo": {
-          "hide_card": false,
-          "background_color": "#b61924",
-          "background_image": "",
-          "background_position": "50% 50%",
-          "background_repeat": "no-repeat",
-          "background_size": "cover"
-        }
-      }
-    );
-
-});
-       ');
+        );
+    });
+');
 ?>
-<style>
-@import url('https://fonts.googleapis.com/css?family=Nunito');
-@import url('https://fonts.googleapis.com/css?family=Poiret+One');
-
-body, html {
-	height: 100%;
-	background-repeat: no-repeat;    /*background-image: linear-gradient(rgb(12, 97, 33),rgb(104, 145, 162));*/
-	background:black;
-	position: relative;
-}
-#login-box {
-	position: absolute;
-	top: 100px;
-	left: 50%;
-	transform: translateX(-50%);
-	width: 350px;
-	margin: 0 auto;
-	border: 1px solid black;
-	background: rgba(48, 46, 45, 1);
-	min-height: 250px;
-	padding: 20px;
-	z-index: 9999;
-        
-}
-#login-box .logo .logo-caption {
-	font-family: 'Poiret One', cursive;
-	color: white;
-	text-align: center;
-	margin-bottom: 0px;
-}
-#login-box .logo .tweak {
-	color: #ff5252;
-}
-
-.login-logo a, .register-logo a{
-    color: white;
-}
-
-#login-box .controls {
-	padding-top: 30px;
-}
-#login-box .controls input {
-	border-radius: 0px;
-	background: rgb(98, 96, 96);
-	border: 0px;
-	color: white;
-	font-family: 'Nunito', sans-serif;
-}
-#login-box .controls input:focus {
-	box-shadow: none;
-}
-#login-box .controls input:first-child {
-	border-top-left-radius: 2px;
-	border-top-right-radius: 2px;
-}
-#login-box .controls input:last-child {
-	border-bottom-left-radius: 2px;
-	border-bottom-right-radius: 2px;
-}
-#login-box button.btn-custom {
-	border-radius: 2px;
-	margin-top: 8px;
-	background:#ff5252;
-	border-color: rgba(48, 46, 45, 1);
-	color: white;
-	font-family: 'Nunito', sans-serif;
-}
-#login-box button.btn-custom:hover{
-	-webkit-transition: all 500ms ease;
-	-moz-transition: all 500ms ease;
-	-ms-transition: all 500ms ease;
-	-o-transition: all 500ms ease;
-	transition: all 500ms ease;
-	background: rgba(48, 46, 45, 1);
-	border-color: #ff5252;
-}
-#particles-js{
-  	width: 100%;
-  	height: 100%;
-  	background-size: cover;
-  	background-position: 50% 50%;
-  	position: fixed;
-  	top: 0px;
-  	z-index:1;
-}    
-</style>
 <div class="container">
 	<div id="login-box">
             <div class="login-logo">
@@ -245,10 +243,9 @@ body, html {
                             ->label(false)
                             ->passwordInput(['placeholder' => $model->getAttributeLabel('Contraseña')]) ?>
 
-                        <?= Html::submitButton('Iniciar', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>    
+                        <?= Html::submitButton('<i class="glyphicon glyphicon-log-in"></i> Iniciar sesion', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>    
                     <?php ActiveForm::end(); ?>
                 </div><!-- /.controls -->
 	</div><!-- /#login-box -->
 </div><!-- /.container -->
 <div id="particles-js"></div>
-<!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></script>-->
