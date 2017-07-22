692086fe7ae592fafb56adf87dbbf46c2a2d79c8
srfrancisco17 <srfrancisco17@gmail.com>
2017-07-22 18:46:53 -0500

    [Commit] - Generando Ejemplo de formulario dinamico en pruebas

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -7,54 +7,61 @@ use Yii;
 
 class PruebasController extends \yii\web\Controller
 {
+
+    
     public function actionIndex()
     {
 
-        $conexion = Yii::$app->db;
-        
-        $barChart = $conexion->createCommand(" 
 
-            select
-                    usu.usuario_id, usu.nombres, usu.apellidos, sum(r.tiempo_desarrollo) as tiempo_total,
-                    (
-                            select
-                                    sum(rt1.horas_desarrollo)
-                            from sprint_requerimientos as sr1
-                            left join sprint_requerimientos_tareas srt1
-                            on(
-                                    sr1.sprint_id = srt1.sprint_id
-                                    and
-                                    sr1.requerimiento_id = srt1.requerimiento_id
-                            )
-                            left join requerimientos_tareas as rt1
-                            on(
-                                    srt1.requerimiento_id = rt1.requerimiento_id
-                                    and rt1.tarea_id = srt1.tarea_id
-                            )
-                            where sr1.sprint_id = 2 and srt1.estado = '4' and sr1.usuario_asignado = usu.usuario_id
-                            group by sr1.usuario_asignado
-                    ) as tiempo_terminado
-            from
-                    sprint_requerimientos as sr
-            inner join requerimientos as r
-            on (
-                    r.requerimiento_id = sr.requerimiento_id
-            )
-            inner join usuarios as usu
-            on (
-                    usu.usuario_id = sr.usuario_asignado
-            )
-            where
-                    sr.sprint_id = 2
-            group by 1, 2, 3, sr.usuario_asignado
-        
-        ")->queryAll();
-        
         
         return $this->render('index', [
 //            'usuario_id' => $usuario_id,
-            'resulado' => $resulado
+
         ]);
+    }
+    
+    public function actionInsertar(){
+        
+    
+    /*
+        $number = count($_POST['name']);
+        
+        //echo $number.'<br>';
+        echo '<pre>';
+        var_dump($_POST['name']);
+        echo '</pre>';
+        if ($number > 0){
+            $conexion = Yii::$app->db;
+            
+            for ($i=0; $i < $number; $i++){
+                
+                if (!empty($_POST['name'][$i])){
+                    
+                $sql = "INSERT INTO comites (comite_alias, lugar) VALUES ('".$_POST['name'][$i]."', '".$_POST['lugar'][$i]."' )";
+
+                 $conexion->createCommand($sql)->execute();
+
+                 echo 'Datos Insertados';
+                    
+                }
+                        
+                
+
+            }
+            
+        }else{
+            echo 'enter name';
+        }
+        
+     */return $this->redirect('index');  
+
+        if (Yii::$app->request->post()){
+            
+            echo 'ESTOY EN EL POST';
+            
+        }else{
+            return $this->redirect('index');  
+        }
     }
 
 }
--- views/pruebas/index.php
+++ views/pruebas/index.php
@@ -2,39 +2,84 @@
 /* @var $this yii\web\View */
 
 use app\models\Usuarios;
-use yii\widgets\Pjax;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
-use kartik\select2\Select2;
 
 
 
+?>
+<?php
+        
+$script = <<< JS
+$(document).ready(function(){
+   var i = 1;
+       
+   $('#add').click(function(){
+      i++; 
+        
+        $('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="name[]" id="name" placeholder="Enter Name" class="form-control name_list"/></td><td><input type="text" name="lugar[]" id="lugar" placeholder="Enter Lugar" class="form-control name_list"/></td><td><button name="remove" id="'+i+'" class = "btn btn-danger btn_remove">X</button></td></tr>');
+        
+   });
+        
+   $(document).on('click', '.btn_remove', function(){
+    
+        var button_id = $(this).attr("id");
+        $("#row"+button_id+"").remove();
+   
+   });
+   
+   /*
+   $('#submit').click(function(){
+        $.ajax({
+        
+            url:"index.php?r=pruebas/insertar",
+            method:"POST",
+            data:$('#add_name').serialize(),
+            succes: function(data)
+            {
+                alert(data);
+                $('#add_name')[0].reset();
+            }
+   
+        });
+   });
+   */     
+});
+JS;
+  
 
-     
-//$array = array(
-//    array('Jorge Rodriguez1',23),
-//    array('Jorge Rodriguez2',23),
-//    array('Jorge Rodriguez3',23),
-//    array('Jorge Rodriguez3',23)
-//);
-
-
+$this->registerJs($script);
 ?>
 
 
 <div class="row">
     
     <div class="col-lg-12">
+        <!--
+        <div class="form-group">
+            <form name="add_name" id="add_name">
+                <table class="table table-bordered" id="dynamic_field">
+                    <tr>
+                        <td><input type="text" name="name[]" id="name" placeholder="Enter Name" class="form-control name_list"/></td>
+                        <td><input type="text" name="lugar[]" id="lugar" placeholder="Enter Lugar" class="form-control name_list"/></td>
+                        <td><button  type="button" name="add" id="add" class="btn-success">Add more</button></td>
+                    </tr>
+                </table>
+                <input type="button" name="submit" id="submit" value="submit" />
+            </form>
+        </div>
+        -->
         
-        <?php
-        
-            echo '<pre>';
-            print_r ($resulado);
-            echo '</pre>';
-            
-            //echo json_encode($array);
-        ?>
-       
+        <div class="form-group">
+            <form name="add_name" id="add_name" method="POST" action="index.php?r=pruebas/insertar">
+                <table class="table table-bordered" id="dynamic_field">
+                    <tr>
+                        <td><input type="text" name="name[]" id="name" placeholder="Enter Name" class="form-control name_list"/></td>
+                        <td><input type="text" name="lugar[]" id="lugar" placeholder="Enter Lugar" class="form-control name_list"/></td>
+                        <td><button  type="button" name="add" id="add" class="btn-success">Add more</button></td>
+                    </tr>
+                </table>
+                <input type="submit" name="submit" id="submit" value="submit" />
+            </form>
+        </div>
     </div>
 
 </div>
