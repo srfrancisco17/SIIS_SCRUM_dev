4acd3079e9934dd6df872282ac22c73521d86e3c
JORGE ALBERTO RODRIGUEZ GONZALEZ <desarrollo6.siis@cdo-sa.com>
2017-08-04 10:29:37 -0500

    ajustes gitignore

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+//Configuracion Postgres-CDO
+return [
+    'class' => 'yii\db\Connection',
+	'dsn' => 'pgsql:host=192.168.101.76;port=5434;dbname=desarrollo_siis',
+	'username' => 'admin',
+	'password' => 'coc70a'
+];
---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+
+//Modo Produccion
+defined('YII_ENV_PROD') or define('YII_ENV_PROD', 'prod');
+defined('YII_DEBUG') or define('YII_DEBUG', FALSE);
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/../config/web.php');
+
+(new yii\web\Application($config))->run();
